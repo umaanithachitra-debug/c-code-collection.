60a7f0249a9df96eca32f88365a8c79af68acc72
Uma Anitha Chitra <umaanithachitra@gmail.com>
2026-07-09 11:18:20 +0530

    Add files via upload

---
+++ arithmetic operation.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a=13,b=7,c;
+	printf("a+b %d \n ",a+b);
+	printf("a-b %d \n",a-b);
+	printf("a*b %d \n",a*b);
+	printf("a/b %d \n",a/b);
+	printf("a%b %d \n",a%b);
+	
+}
---
+++ assignment operations.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a=5,c,d;
+	c=a;
+	printf("c %d \n",c);
+	c+=a;
+	printf("c %d \n",c);
+	c-=a;
+	printf("c %d \n",c);
+	c*=a;
+	printf("c %d \n",c);
+	c/=a;
+	printf("c %d \n",c);
+	c%=a;
+	printf("c %d ",c);
+	
+}
