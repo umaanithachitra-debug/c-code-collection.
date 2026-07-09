b802a42f4b9fda2b4a77a4511bd071717b9f6b11
Uma Anitha Chitra <umaanithachitra@gmail.com>
2026-07-09 10:57:30 +0530

    Add files via upload

---
+++ expression.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	float c;
+	double d;
+	 int e=(a/b*c)-b+(a*d/3);
+	printf("enter a,b value:%d%d",a,b);
+	scanf("d%d",a,b);
+	printf("enter c value:%f",c);
+	scanf("%f",c);
+	printf("enter d value :%lf",d);
+	scanf("%lf",d);
+	printf("the value of e:%d",e);
+}
