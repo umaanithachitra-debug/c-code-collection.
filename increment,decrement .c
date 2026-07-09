217df6e6da2781e3cfd317e1fed817cdd024f691
Uma Anitha Chitra <umaanithachitra@gmail.com>
2026-07-09 11:15:57 +0530

    Add files via upload

---
+++ increment,decrement .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   int a=11, b=10;
+   float c=9.5, d=10.5;
+   printf("++a=%d \n ",++a);
+   printf("--b=%d \n ",--b);
+   printf("++c=%f \n ",++c);
+   printf("--d=%f \n ",--d);
+   	
+}
